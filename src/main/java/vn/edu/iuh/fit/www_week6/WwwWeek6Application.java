e64bc79f7bf48334061bfcee9b671a6df5549e2b
TranThienDatLaGi <tranthiendatlagi@gmail.com>
2023-11-03 15:57:50 +0700

    create

---
+++ src/main/java/vn/edu/iuh/fit/www_week6/WwwWeek6Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_week6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwWeek6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwWeek6Application.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/www_week6/WwwWeek6ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_week6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwWeek6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
